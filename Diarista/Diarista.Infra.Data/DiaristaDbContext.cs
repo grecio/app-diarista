faa41735e016b8d07a62ad7e653bf63118c3dd16
Zozimo.Alexandre <zozimo.alexandre@gmail.com>
2021-01-07 23:34:04 -0300

    Concluindo a camada data

---
+++ Diarista/Diarista.Infra.Data/DiaristaDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Diarista.Infra.Data
+{
+    public class DiaristaDbContext : DbContext
+    {
+        public DiaristaDbContext()
+        {
+            //
+        }
+
+        public DiaristaDbContext(DbContextOptions<DiaristaDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ Diarista/Diarista.Infra.Data/DiaristaDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Diarista.Infra.Data
+{
+    class DiaristaDbContextFactory : IDesignTimeDbContextFactory<DiaristaDbContext>
+    {
+    
+        public DiaristaDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<DiaristaDbContext>();
+
+            optionsBuilder.UseSqlServer("Server=144.217.49.13,1433;Database=DiaristaDb;User Id=sa;Password=buga@tec_buga1;");
+
+            return new DiaristaDbContext(optionsBuilder.Options);
+        }
+    }
+}
